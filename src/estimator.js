5de6860e160356f76f4b3210e061f1a646989e98
Engr Dauda <dauda933@gmail.com>
2020-04-10 17:54:27 +0100

    after eslint

--- src/estimator.js
+++ src/estimator.js
@@ -1,117 +1,86 @@
 const covid19ImpactEstimator = (data) => {
+  const input = data;
+  function fifteenpercent(digit) {
+    return ((15 / 100) * digit);
+  }
+  function fivePercent(digits) {
+    return ((5 / 100) * digits);
+  }
+  const noOfDays = (time, format) => {
+    let result = 0;
+    if (format === 'days') {
+      result = time;
+    } else if (format === 'days') {
+      result = time * 7;
+    } else if (format === 'months') {
+      result = time * 30;
+    }
+    return result;
+  };
 
-    const input = data;
-
-   function fifteenpercent(digit){
-
-       return ((15/100) *digit);
-       
-   }
-
-   function thirthyfivepercent(){
-       return  ((35/100) *data.totalHospitalBeds);
-   }
-
-   function fivePercent(digits){
-       return ((5/100) * digits);
-   }
-
-   function twoPercent(digits){
-    return ((2/100) * digits);
-}
-   let nth = (days) =>{ 
+  function twoPercent(digits) {
+    return ((2 / 100) * digits);
+  }
+  const nth = (days) => {
     let factor = 0;
-    let total = 0
-    days = noOfDays(days, data.periodType);
-    while(3 + total <= days){
-        factor +=1
-        total = total +3
-       
-     }
-     return factor;
+    let total = 0;
+    const day = noOfDays(days, data.periodType);
+    while (3 + total <= day) {
+      factor += 1;
+      total += 3;
     }
+    return factor;
+  };
 
-    let noOfDays = (time, format) =>{
-        result = 0;
-        switch(format){
-            case "days":
-                result = time;
-                break;
-            case "weeks":
-                result = time * 7;
-                break;
-            case "months":
-                result = time *30;
-                break;
-        }
-        return result;
 
+  const currentlyInfected1 = data.reportedCases * 10;
+  const severeCurrentlyInfected = data.reportedCases * 50;
+
+  const infectionsByRequestedTime1 = currentlyInfected1 * (2 ** (nth(data.timeToElapse)));
+  const infectionsByRequestedTime2 = severeCurrentlyInfected * (2 ** (nth(data.timeToElapse)));
+
+  const severeCasesByRequestedTime1 = fifteenpercent(infectionsByRequestedTime1);
+  const severeCasesByRequestedTime2 = fifteenpercent(infectionsByRequestedTime2);
+
+  const hospitalBedsByRequested = (digit) => digit - data.totalHospitalBeds;
+
+  const hospitalBedsByRequestedTime1 = hospitalBedsByRequested(severeCasesByRequestedTime1);
+  const hospitalBedsByRequestedTime2 = hospitalBedsByRequested(severeCasesByRequestedTime2);
+
+  const casesForICUByRequestedTime1 = fivePercent(infectionsByRequestedTime1);
+  const casesForICUByRequestedTime2 = fivePercent(infectionsByRequestedTime2);
+
+  const casesForVentilatorsByRequestedTime1 = twoPercent(infectionsByRequestedTime1);
+  const casesForVentilatorsByRequestedTime2 = twoPercent(infectionsByRequestedTime2);
+
+  const multiples = (digits) => {
+    const constants = data.avgDailyIncomePopulation * noOfDays(data.timeToElapse, data.periodType);
+    return ((digits * constants * data.avgDailyIncomeInUSD).toFixed(2));
+  };
+  const d1 = multiples(infectionsByRequestedTime1);
+  const d2 = multiples(infectionsByRequestedTime2);
+  return {
+    data: input,
+    impact: {
+      currentlyInfected: currentlyInfected1,
+      infectionsByRequestedTime: infectionsByRequestedTime1,
+      severeCasesByRequestedTime: severeCasesByRequestedTime1,
+      hospitalBedsByRequestedTime: hospitalBedsByRequestedTime1,
+      casesForICUByRequestedTime: casesForICUByRequestedTime1,
+      casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime1,
+      dollarsInFlight: d1
+    },
+
+    severeImpact: {
+      currentlyInfected: severeCurrentlyInfected,
+      infectionsByRequestedTime: infectionsByRequestedTime2,
+      severeCasesByRequestedTime: severeCasesByRequestedTime2,
+      hospitalBedsByRequestedTime: hospitalBedsByRequestedTime2,
+      casesForICUByRequestedTime: casesForICUByRequestedTime2,
+      casesForVentilatorsByRequestedTime: casesForVentilatorsByRequestedTime2,
+      dollarsInFlight: d2
     }
-    let impact ={
-
-    }
-
-    let severeImpact = {
-
-    }
-
-    let currentlyInfected1 = data.reportedCases * 10;
-    let severeCurrentlyInfected = data.reportedCases * 50;
-
-    let infectionsByRequestedTime1 = currentlyInfected1 * (Math.pow(2, nth(data.timeToElapse)));
-    let severeInfectionsByRequestedTime = severeCurrentlyInfected * (Math.pow(2, nth(data.timeToElapse)));
-
-    let severeCasesByRequestedTime1 = fifteenpercent(infectionsByRequestedTime1);
-    let severeCasesByRequestedTime2 = fifteenpercent(severeInfectionsByRequestedTime);
-
-    let hospitalBedsByRequested = (digit)=>{
-        return digit - totalHospitalBeds;
-    }
-
-    let hospitalBedsByRequestedTime1 = hospitalBedsByRequested(severeCasesByRequestedTime1);
-    let hospitalBedsByRequestedTime2 = hospitalBedsByRequested(severeCasesByRequestedTime2);
-
-    let casesForICUByRequestedTime1 = fivePercent(infectionsByRequestedTime1);
-    let casesForICUByRequestedTime2 = fivePercent(severeInfectionsByRequestedTime);
-
-    let casesForVentilatorsByRequestedTime1 = twoPercent(infectionsByRequestedTime1);
-    let casesForVentilatorsByRequestedTime2 = twoPercent(severeInfectionsByRequestedTime);
-
-    let dollarsInFlight1 = (infectionsByRequestedTime1 * data.avgDailyIncomeInUSD * data.avgDailyIncomePopulation * noOfDays(data.timeToElapse, data.periodType)).toFixed(2);
-    let dollarsInFlight2 = (infectionsByRequestedTime2 * data.avgDailyIncomeInUSD * data.avgDailyIncomePopulation * noOfDays(data.timeToElapse, data.periodType)).toFixed(2);
-
-
-
-    return {
-        data : input,
-        impact:{
-            currentlyInfected : currentlyInfected1, 
-            infectionsByRequestedTime : infectionsByRequestedTime1,
-            severeCasesByRequestedTime: severeCasesByRequestedTime1,
-            hospitalBedsByRequestedTime : hospitalBedsByRequestedTime1,
-            casesForICUByRequestedTime : casesForICUByRequestedTime1,
-            casesForVentilatorsByRequestedTime : casesForVentilatorsByRequestedTime1,
-            dollarsInFlight : dollarsInFlight1
-        },
-
-        severeImpact: {
-            currentlyInfected : severeCurrentlyInfected,
-            infectionsByRequestedTime : severeInfectionsByRequestedTime,
-            severeCasesByRequestedTime : severeCasesByRequestedTime2,
-            hospitalBedsByRequestedTime : hospitalBedsByRequestedTime2,
-            casesForICUByRequestedTime : casesForICUByRequestedTime2 ,
-            casesForVentilatorsByRequestedTime : casesForVentilatorsByRequestedTime2, 
-            dollarsInFlight : dollarsInFlight2
-        }
-    }
-
-
-
-    
-
-
-    
-
-}
+  };
+};
 
 export default covid19ImpactEstimator;
